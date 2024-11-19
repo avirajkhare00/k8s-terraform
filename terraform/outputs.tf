bac1bdb7cea8c910c73730e8776df7598c6995c6
avirajkhare00 <avirajkhare00@gmail.com>
2024-11-20 02:06:45 +0530

    [Aviraj] Fix outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,14 +3,16 @@ data "kubernetes_service" "ingress_nginx" {
     name      = "ingress-nginx-controller"
     namespace = var.ingress_namespace
   }
+
+  depends_on = [helm_release.ingress_nginx]
 }
 
 output "ingress_ip" {
   description = "The external IP address of the Ingress Controller."
-  value       = data.kubernetes_service.ingress_nginx.status[0].load_balancer[0].ingress[0].ip
+  value       = data.kubernetes_service.ingress_nginx.status.load_balancer.ingress[0].ip
 }
 
 output "hello_world_url" {
   description = "The URL to access the Hello World application."
-  value       = "http://${data.kubernetes_service.ingress_nginx.status[0].load_balancer[0].ingress[0].ip}/"
+  value       = "http://${data.kubernetes_service.ingress_nginx.status.load_balancer.ingress[0].ip}/"
 }
