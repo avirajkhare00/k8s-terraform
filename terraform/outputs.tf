51d310c23227c0cc955733ba4dd2bbf760d0517a
avirajkhare00 <avirajkhare00@gmail.com>
2024-11-20 02:11:58 +0530

    [Aviraj] Fixed value in outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,10 +9,10 @@ data "kubernetes_service" "ingress_nginx" {
 
 output "ingress_ip" {
   description = "The external IP address of the Ingress Controller."
-  value       = data.kubernetes_service.ingress_nginx.status.load_balancer.ingress[0].ip
+  value       = data.kubernetes_service.ingress_nginx.load_balancer_ingress[0].ip
 }
 
 output "hello_world_url" {
   description = "The URL to access the Hello World application."
-  value       = "http://${data.kubernetes_service.ingress_nginx.status.load_balancer.ingress[0].ip}/"
+  value       = "http://${data.kubernetes_service.ingress_nginx.load_balancer_ingress[0].ip}/"
 }
