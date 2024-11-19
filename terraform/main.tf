9e4944ff9771dd6a7dcc2090e3697c1d673109f6
avirajkhare00 <avirajkhare00@gmail.com>
2024-11-20 02:04:00 +0530

    [Aviraj] Fixed https issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,7 +72,7 @@ output "kubernetes_cluster_ca_certificate" {
 provider "kubernetes" {
   host                   = "https://${google_container_cluster.primary.endpoint}"
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
-  token                  = data.google_client_config.access_token
+  token                  = data.google_client_config.default.access_token
 }
 
 data "google_client_config" "default" {}
